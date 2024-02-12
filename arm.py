637300858e146f1f5b2cb7e3d95a4e7039f21459
sem@frankapc <sem@teknologisk.dk>
2024-02-12 15:52:00 +0100

    Added some helper functions

--- arm.py
+++ arm.py
@@ -1,16 +1,31 @@
 import moveit_commander
 import rospy
-from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import WrenchStamped, Pose, Quaternion
 from franka_msgs.msg import ErrorRecoveryActionGoal, FrankaState
 from franka_msgs.srv import SetForceTorqueCollisionBehavior
 from time import sleep
 from .gripper import GripperInterface
+import tf.transformations
 
 DEBUG = False
 
 def rprint(msg):
     if DEBUG:
-        rospy.logdebug(msg)
+
+def eulerToQuaternion(current_orientation:Quaternion, x, y, z):
+    q2=tf.transformations.quaternion_from_euler(x,y,z)
+
+    quaternion=tf.transformations.quaternion_multiply(q2,[current_orientation.x, current_orientation.y, current_orientation.z, current_orientation.w])
+
+    # quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+    ori = Quaternion(x=quaternion[0], y=quaternion[1], z=quaternion[2], w=quaternion[3])
+
+    return ori
+
+def quaternionToEuler(quaternion: Quaternion):
+    euler = tf.transformations.euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w] )
+    
+    return euler
 
 class PandaArm():
     def __init__(self):
